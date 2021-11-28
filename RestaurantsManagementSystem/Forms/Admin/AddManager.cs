cfeb342ad63f5213cf08b194e4768dd9783c646f
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-11-28 16:17:33 +0600

    random generate id and password for manager

--- RestaurantsManagementSystem/Forms/Admin/AddManager.cs
+++ RestaurantsManagementSystem/Forms/Admin/AddManager.cs
@@ -1,5 +1,6 @@
 ﻿using Database;
 using Database.Entities;
+using Presentation.RandomSample;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,17 +16,23 @@ namespace Presentation.Forms.Admin
     public partial class AddManager : Form
     {
         DataAccess dataAccess = new DataAccess();
+        RandomGenerator randomGenerator = new RandomGenerator();
         public AddManager()
         {
             InitializeComponent();
             panelAddManagerInformation.Enabled = false;
-            //txtUserId.Enabled = false;
-            //txtPassword.Enabled = false;
+            txtUserId.Enabled = false;
+            txtPassword.Enabled = false;
+            string userId = randomGenerator.RandomString(5, true);
+            string password = randomGenerator.RandomPassword();
+            txtUserId.Text = userId;
+            txtPassword.Text = password;
         }
 
         private void btnAddManagerIdpass_Click(object sender, EventArgs e)
         {
             panelAddManagerInformation.Enabled = true;
+            
             int userType = (int)User.UserTypeEnum.Manager;
             bool isUserInsert = dataAccess.InsertUser(txtUserId.Text, txtPassword.Text, userType);
             if (isUserInsert)
